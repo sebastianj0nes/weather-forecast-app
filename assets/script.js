2f7e37f8fef5ec406e6684ee3cba3b439ae3f778
Seb <jonesseb10@gmail.com>
2023-01-29 21:15:52 +0000

    update functionality on today forecast, adding header using time and location

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,7 @@
 // Initialise javascript variables
 var textInput;
 var submitButton = $("#search-button");
+var todaySec = $("#today");
 
 // Initialise API
 var APIKey = "29f595125610436376375721ee1541b8";
@@ -12,23 +13,43 @@ submitButton.on("click",function(event){
 
     console.log(textInput);
 
-    getData();
+    todayForecast();
 })
-
-// Build URL to search database
-var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + textInput + "&appid=" + APIKey;
 
 
 // Func to get data from OpenWeather
-var getData = function (){
+var todayForecast = function (){
 
-    // Ajax call to retrieve data using API
-    $.ajax({
+    // Ajax call to retrieve co-ordinates from using API
+    var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q="+ textInput+ "&appid=" + APIKey;
+      $.ajax({
         url: queryURL,
         method: "GET"
       }).then(function(response) {
-      console.log(response);
+        console.log(response);
+
+        // Initialise variables to store relevant info
+        var todayTemp = response.list[0].main.temp;
+        var todayWind = response.list[0].wind.speed;
+        var todayHumid = response.list[0].main.humidity;
+        var todayTime = moment().format("D/M/YY");
 
 
-});
-}
+        var todayHeader = $("<h1>");
+        todayHeader.text("Current forecast in " + textInput+" @ " + todayTime);
+        console.log(todayHeader);
+        todaySec.append(todayHeader);
+
+
+        console.log("Temp: " + todayTemp);
+        console.log("Wind: " + todayWind);
+        console.log("Humidity: " + todayHumid + " %");
+
+
+
+
+
+
+
+      });
+    }
